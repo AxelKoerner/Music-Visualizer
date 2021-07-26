e001fb384f33ba000c6bb7e4bf720d8fb985a36e
AxelKoerner <91koax1bif@hft-stuttgart.de>
2021-07-26 15:06:04 +0200

    added new visualizer mode

--- src/main.cpp
+++ src/main.cpp
@@ -13,15 +13,18 @@
 #define COLOR_ORDER   GRB
 #define THRESHOLD     80
 #define DELAY         5
-#define DECAY         255
+#define DECAY         100
+#define POWERINMILLIWATTS 300
 
 int leftAudio[7];
 int rightAudio[7];
 int frequency;
+int frequencyBand;
 int frequencyRange[7];
 int mid = NUMB_LEDS / 2;
 uint8_t hue = 0;                                      //uint8 type because it has a range to 255 and if we increment this variable over 255 it jumps back to 0, usefull for rainbow effect
 int brightness = 100;
+
 
 CRGB blue    =    CRGB(0, 0, 255);
 CRGB red     =    CRGB(255, 0, 0);
@@ -77,28 +80,31 @@ CRGB Scroll(int pos) {
 }
 
 
-void frequencyGetterBand0() {
-  frequency = (leftAudio[0] + rightAudio[0]) / 2;
-}
-
-void frequencyGetterBand1() {
-  frequency = (leftAudio[1] + rightAudio[1]) / 2;
+void frequencyGetterSingleBand(int frequencyBand) {
+  frequency = (leftAudio[frequencyBand] + rightAudio[frequencyBand]) / 2;
 }
 
 void frequencyGetterBandLow() {
   frequency = (leftAudio[0] + rightAudio[0] + leftAudio[1] + rightAudio[1])/ 4;
 }
 
-static const CRGB RandomColors [10] = 
+void frequencyGetterAllBands() {
+  for(int i = 0; i < 7; i++) {
+    frequencyRange[i] = (leftAudio[i] + rightAudio[i]) / 2;
+  }
+}
+
+static const CRGB RandomColors [11] = 
 {
-  CRGB::Red,
-  CRGB::Blue,
+  CRGB::White,
+  CRGB::DarkBlue,
+  CRGB::SkyBlue,
   CRGB::Green,
   CRGB::Yellow,
   CRGB::Orange,
-  CRGB::Purple,
+  CRGB::Red,
   CRGB::Pink,
-  CRGB::White,
+  CRGB::FloralWhite,
   CRGB::SeaGreen,
   CRGB::PaleVioletRed
 };
@@ -108,7 +114,7 @@ static const CRGB RandomColors [10] =
 
 void SingleBand0RedBlue() {                                               //1) lowest frequency band, single direction from the start of the led strip, Red and Blue
   readICs();  
-  frequencyGetterBand1();                            
+  frequencyGetterSingleBand(1);                            
   for(int i = 0; i < NUMB_LEDS; i++) {
     if (i < (frequency - THRESHOLD)) {
       leds[i] = CRGB(BRIGHTNESS, 0,BRIGHTNESS);
@@ -122,7 +128,7 @@ void SingleBand0RedBlue() {                                               //1) l
 
 void fadeToBlackTester() {
   readICs();  
-  frequencyGetterBand1();                            
+  frequencyGetterSingleBand(1);                            
   for(int i = 0; i < NUMB_LEDS; i++) {
     if (i < (frequency - THRESHOLD)) {
       leds[i] = yellow;
@@ -135,9 +141,9 @@ void fadeToBlackTester() {
 }
 
   
-void SingleBand1Piping() {
+ const void SingleBand1Piping() {
   readICs();
-  frequencyGetterBand1();
+  frequencyGetterSingleBand(1);
   for(int i = NUMB_LEDS - 1; i >= 3; i--) {
     leds[i] = leds[i - 3]; 
     leds[i-3].fadeToBlackBy(DECAY); 
@@ -167,6 +173,26 @@ void AllBands() {                                                     // checks 
 }
 
 
+void AllBandsPiping() {
+  readICs();
+  frequencyGetterAllBands();
+  for(int i = NUMB_LEDS - 1; i >= 3; i--) {
+    leds[i] = leds[i - 3]; 
+    leds[i-3].fadeToBlackBy(DECAY); 
+  }
+  for(int i = 0; i < 7; i++) {
+    if(frequencyRange[i] > THRESHOLD) {
+      CRGB newColor = RandomColors[i];
+      leds[0] = newColor;
+      leds[1] = newColor;
+      leds[2] = newColor;
+    } 
+  }
+  FastLED.show();
+  delay(DELAY);
+}
+
+
 void Rainbow() {                                                     // Rainbow on whole led strip, changes color by increasing hue variable
    for(int i = 0; i < NUMB_LEDS; i++) {
       leds[i] = CHSV(hue + (i * 10), 255, brightness);
@@ -180,7 +206,7 @@ void Rainbow() {                                                     // Rainbow 
 
 void RainbowReactiveBand0() {
   readICs();
-  frequencyGetterBand1();
+  frequencyGetterSingleBand(1);
    if(frequency > THRESHOLD) {
     for(int i = 0; i < NUMB_LEDS; i++) {
        leds[i] = CHSV(hue + (i * 10), 255, 200);
@@ -211,7 +237,6 @@ void Random() {
 void Twinkle() {
   static int counter = 0;
   counter ++;
-  
   if(counter == NUMB_LEDS)  {
     counter = 0;
     FastLED.clear(false);
@@ -225,19 +250,21 @@ void Twinkle() {
 //-----------------------------------------------SETUP-----------------------------------------------//
 
 void setup() {
-  pinMode(DC_One, INPUT);     //using A0 and A1 as inputs to read out the 7 frequency bands 
-  pinMode(DC_Two, INPUT);     //from the Sparkfun Spectum Shield. A0 is the DC analog output 
-                              //from the first IC for the left audio channel, A1 is the output
-                              //for the second IC for the right audio channel
-  pinMode(STROBE, OUTPUT);    //pin 4 is the Strobe pin, it cycles through each frequency channel once activated
-  pinMode(RESET, OUTPUT);     //pin 5 is the reset pin for out Sparfun Board
-  digitalWrite(STROBE, LOW);  //we write Low to the strobe pin so that is doesnt cycle when we start our program
-  digitalWrite(RESET, HIGH);  //reset pin to high, resets entire multiplexer 
+  pinMode(DC_One, INPUT);                                     //using A0 and A1 as inputs to read out the 7 frequency bands 
+  pinMode(DC_Two, INPUT);                                     //from the Sparkfun Spectum Shield. A0 is the DC analog output 
+                                                              //from the first IC for the left audio channel, A1 is the output
+                                                              //for the second IC for the right audio channel
+  pinMode(STROBE, OUTPUT);                                    //pin 4 is the Strobe pin, it cycles through each frequency channel once activated
+  pinMode(RESET, OUTPUT);                                     //pin 5 is the reset pin for out Sparfun Board
+  digitalWrite(STROBE, LOW);                                  //we write Low to the strobe pin so that is doesnt cycle when we start our program
+  digitalWrite(RESET, HIGH);                                  //reset pin to high, resets entire multiplexer 
 
   delay(3000);
   FastLED.addLeds<LED_TYPE, LED_PIN, GRB>(leds, NUMB_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(BRIGHTNESS);
   clearLEDs();
+  FastLED.setBrightness(BRIGHTNESS);
+  FastLED.setMaxPowerInMilliWatts(POWERINMILLIWATTS);
 
 }
 
@@ -245,7 +272,8 @@ void setup() {
 //-----------------------------------------------EVENT LOOP-----------------------------------------------//
 
 void loop() {
- //SingleBand0RedBlue();
+
+
  SingleBand1Piping();
  //AllBands();
  //Rainbow();
@@ -253,4 +281,6 @@ void loop() {
  //Random();
  //fadeToBlackTester();
  //Twinkle();
+ //AllBandsPiping();
+
 }
